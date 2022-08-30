d18c5ce5f80a9aeba3024db8eb4dafefbc977e8c
DmitryGrigoryev <grigoryev.dima@gmail.com>
2022-08-30 18:36:37 -0400

    initial comit
    project creation

---
+++ engine-service/src/test/java/com/grigoryev/engineservice/EngineServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grigoryev.engineservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngineServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
